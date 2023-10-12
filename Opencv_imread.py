b85c576d065d7128e99b6f3d9d1dc29a32932d59
arshiya <arshiyajafari.sh@gmail.com>
2023-10-12 19:39:50 +0330

    imread

---
+++ Opencv_imread.py
@@ -0,0 +1,10 @@
+#import modules.
+import cv2 as cv
+import matplotlib.pyplot as plt
+
+#Reading image.
+#We can pass 0 as flags and get a grayscale picture.
+image = cv.imread("File_addres")
+#After that we can display image
+plt.imshow()
+plt.show()
